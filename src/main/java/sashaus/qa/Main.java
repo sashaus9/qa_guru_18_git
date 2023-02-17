e59e8badeaf53892e3fc8370f1ea3c35f65af1ad
sashaus9 <100185391+sashaus9@users.noreply.github.com>
2023-02-17 17:44:01 +0300

    Update Main.java

--- src/main/java/sashaus/qa/Main.java
+++ src/main/java/sashaus/qa/Main.java
@@ -2,7 +2,7 @@ package sashaus.qa;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello!";
+        String message = "Hi!";
         System.out.println(message);
     }
 }
